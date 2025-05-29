8601284f93391f419bb89c92c9acfdc9b29e202c
kevamrein <kevamrein@gmail.com>
2025-05-29 07:54:07 -0400

    Updating script to account for new investor field

--- src/app/(payload)/api/update-investment-recommendations/route.ts
+++ src/app/(payload)/api/update-investment-recommendations/route.ts
@@ -64,7 +64,7 @@ export async function GET(request: Request) {
       const investments = await payload.find({
         collection: 'investment',
         where: {
-          investor: {
+          investorMapping: {
             equals: investor.id,
           },
         },
