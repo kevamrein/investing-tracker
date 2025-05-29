10d562312f1164b919138390246817b7f6d46c2b
kevamrein <kevamrein@gmail.com>
2025-05-29 07:53:51 -0400

    Adding migration for new field

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250526_154737 from './20250526_154737';
+import * as migration_20250529_115021_investor_on_investment from './20250529_115021_investor_on_investment';
 
 export const migrations = [
   {
     up: migration_20250526_154737.up,
     down: migration_20250526_154737.down,
-    name: '20250526_154737'
+    name: '20250526_154737',
+  },
+  {
+    up: migration_20250529_115021_investor_on_investment.up,
+    down: migration_20250529_115021_investor_on_investment.down,
+    name: '20250529_115021_investor_on_investment'
   },
 ];
