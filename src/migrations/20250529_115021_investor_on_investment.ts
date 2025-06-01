b8b4f009df92bfec17d004af6571b858953a7bac
kevamrein <kevamrein@gmail.com>
2025-06-01 17:51:30 -0400

    Adding default 1 for migration

--- src/migrations/20250529_115021_investor_on_investment.ts
+++ src/migrations/20250529_115021_investor_on_investment.ts
@@ -2,7 +2,7 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "investment" ADD COLUMN "investor_mapping_id" integer NOT NULL;
+   ALTER TABLE "investment" ADD COLUMN "investor_mapping_id" integer DEFAULT 1 NOT NULL;
   DO $$ BEGIN
    ALTER TABLE "investment" ADD CONSTRAINT "investment_investor_mapping_id_investors_id_fk" FOREIGN KEY ("investor_mapping_id") REFERENCES "public"."investors"("id") ON DELETE set null ON UPDATE no action;
   EXCEPTION
