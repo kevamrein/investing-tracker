2f808c428f8965ae4df2cfbea369e1feab1e047d
kevamrein <kevamrein@gmail.com>
2025-10-02 19:33:31 -0400

    Adding account type migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250526_154737 from './20250526_154737';
 import * as migration_20250529_115021_investor_on_investment from './20250529_115021_investor_on_investment';
+import * as migration_20251002_233302_add_account_type from './20251002_233302_add_account_type';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250529_115021_investor_on_investment.up,
     down: migration_20250529_115021_investor_on_investment.down,
-    name: '20250529_115021_investor_on_investment'
+    name: '20250529_115021_investor_on_investment',
+  },
+  {
+    up: migration_20251002_233302_add_account_type.up,
+    down: migration_20251002_233302_add_account_type.down,
+    name: '20251002_233302_add_account_type'
   },
 ];
