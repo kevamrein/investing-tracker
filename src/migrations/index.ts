751f703c6bf1249e6757ba32cd6f4565c22fe2d9
kevamrein <kevamrein@gmail.com>
2025-11-06 22:01:35 -0500

    Add investable assets migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20250526_154737 from './20250526_154737';
 import * as migration_20250529_115021_investor_on_investment from './20250529_115021_investor_on_investment';
 import * as migration_20251002_233302_add_account_type from './20251002_233302_add_account_type';
+import * as migration_20251107_030056_investable_assets from './20251107_030056_investable_assets';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20251002_233302_add_account_type.up,
     down: migration_20251002_233302_add_account_type.down,
-    name: '20251002_233302_add_account_type'
+    name: '20251002_233302_add_account_type',
+  },
+  {
+    up: migration_20251107_030056_investable_assets.up,
+    down: migration_20251107_030056_investable_assets.down,
+    name: '20251107_030056_investable_assets'
   },
 ];
